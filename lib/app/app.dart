dee3c730a01d20ad912a8464ad62f869f5449641
Daniel Alejandro Munoz <damuoz121@soy.sena.edu.co>
2023-08-05 14:16:48 -0500

    Se creo un nuevo proyecto

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:sekerme_ecommerce/app/config/router/app_route.dart';
 
 import 'config/themes/theme.dart';
-import 'presentation/views/views_links.dart';
+
 
 
 class App extends StatelessWidget {
   const App({super.key});
 
+  get appRouter => null;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedThemeColor: 0).theme(),
-      home: const LoginView(),
     );
   }
 }
--- lib/app/presentation/views/register/register_view.dart
+++ lib/app/presentation/views/register/register_view.dart
@@ -1,13 +1,68 @@
 import 'package:flutter/material.dart';
 
-class RegisterView extends StatelessWidget {
 
-  static const String name = 'register_view';
+void main() {
+  runApp(const MyApp());
+}
 
-  const RegisterView({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Página Celular'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                // Acción para el primer botón
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              ),
+              child: const Text(
+                'Botón 1',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acción para el segundo botón
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              ),
+              child: const Text(
+                'Botón 2',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
